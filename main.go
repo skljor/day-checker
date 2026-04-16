fa5fa872077b564f78e03da8fa1dd0cbf0c34702
Nikolay <k.nikolay-kk@yandex.ru>
2026-04-16 13:43:57 +0500

    feat: add error handling
    Add input check
    
    Handle non-numeric input
    
    Prevent division by zero for height

--- main.go
+++ main.go
@@ -9,10 +9,23 @@ import (
 func main() {
 	var testUser models.User
 
-	fmt.Println("Enter your height: ")
-	fmt.Scanln(&testUser.Height)
-	fmt.Println("Enter your weight: ")
-	fmt.Scanln(&testUser.Weight)
+	for {
+		fmt.Print("What is your height(m) and weight(kg): ")
+
+		if _, err := fmt.Scan(&testUser.Height, &testUser.Weight); err != nil {
+			fmt.Println("Parameters should be numerical")
+			//clear the input buffer
+			var trash string
+			fmt.Scanln(&trash)
+			continue
+		}
+		if testUser.Height <= 0 || testUser.Weight <= 0 {
+			fmt.Println("Error: Height and weight must be greater than zero.")
+			continue
+		}
+
+		break
+	}
 	testUser.Print()
 	bmi, status := testUser.BMI()
 	fmt.Printf("Your index: %.2f. You are %s\n", bmi, status)
